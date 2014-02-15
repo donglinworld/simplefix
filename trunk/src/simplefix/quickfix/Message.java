28fe9a298a7e1d682ea29b57b17340685d0aecf8
donglin_world@hotmail.com <donglin_world@hotmail.com@b05cb261-0605-1b5b-92d6-37ff6522ea18>
2014-02-15 06:17:33 +0000

    1.0.4 update

--- trunk/src/simplefix/quickfix/Message.java
+++ trunk/src/simplefix/quickfix/Message.java
@@ -110,4 +110,8 @@ public class Message implements simplefix.Message {
         _msg.setGroups(tag.getTagNum(), quickGroups);
     }
 
+    @Override
+    public String toString() {
+        return _msg.toString();
+    }
 }
